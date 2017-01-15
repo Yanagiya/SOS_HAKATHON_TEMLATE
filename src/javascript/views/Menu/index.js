06486c233bce47895e0232376b97bf09681518e3
shin-kinoshita <shin.kinoshita@nextremer.com>
2017-01-15 13:37:22 +0530

    updated

--- src/javascript/views/Menu/index.js
+++ src/javascript/views/Menu/index.js
@@ -7,6 +7,7 @@ import { Link } from 'react-router';
 import ContentAdd from 'material-ui/lib/svg-icons/content/add';
 import Post from './Post';
 import * as BlogActions from '../../redux/modules/blogposts';
+import menuInfos from './urls';
 
 let confirmLink = <Link to="/confirm" />;
 
@@ -41,25 +42,34 @@ class BlogApp extends Component {
     console.log(e);
     console.log("#############");
   }
+
+  renderImg() {
+    var rows = [];
+    var loc = window.location.pathname;
+    var dir = loc.substring(0, loc.lastIndexOf('/'));
+    for ( var i = 0; i < menuInfos.length; i++ ) {
+      rows.push(
+        <div>
+          <img src={menuInfos[i].url} />
+          <RaisedButton name="btn" label="order" primary={true} style={style} onClick={this.orderProcess.bind(this)} />
+          <RaisedButton name="btn" label="cancel" secondary={true} style={style} disabled="true" onClick={this.orderProcess.bind(this)} />
+        </div>
+      );
+    }
+    return rows;
+  }
+
   render() {
     const { history } = this.context;
     const { blogposts, users, dispatch } = this.props;
     const actions = bindActionCreators(BlogActions, dispatch);
     const styles = this.getStyles();
+    const imgParts = this.renderImg();
 
     return (
         <AppBar>
-        <RaisedButton label="CONFIRM" primary={true} style={style} containerElement={confirmLink}/>            
-          {blogposts.map((post, i) =>
-              <div>
-                  <Post key={i}
-                        post={post}
-                        user={users.filter(user => user.id === post.user)[0]}
-                        actions={actions}/>
-                  <RaisedButton name="btn" label="order" primary={true} style={style} onClick={this.orderProcess.bind(this)} />
-                  <RaisedButton name="btn" label="cancel" secondary={true} style={style} disabled="true" onClick={this.orderProcess.bind(this)} />
-              </div>
-          )}
+          <RaisedButton label="CONFIRM" primary={true} style={style} containerElement={confirmLink}/>            
+          <div>{imgParts}</div>
         </AppBar>
     );
   }
